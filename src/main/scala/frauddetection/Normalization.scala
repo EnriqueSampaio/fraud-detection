13c07294bff4dbed9afd71aa2c6095102c8cc7a6
Enrique Sampaio dos Santos <developer@enriquesampaio.com>
2017-09-15 22:33:41 -0300

    Adding target attribute to the end of normalized tuple

--- src/main/scala/frauddetection/Normalization.scala
+++ src/main/scala/frauddetection/Normalization.scala
@@ -12,28 +12,29 @@ object Normalization {
     val sc = new SparkContext(conf)
 
     val rows = sc.textFile("resources/creditcard.csv")
-      .map(row => row.split(",").slice(0,29).map(value => value.toDouble))
+      .map(row => row.split(","))
+      .map(row => (row(30), row.slice(0,30).map(value => value.toDouble)))
     val count = rows.count()
 
-    val means = rows.reduce{ case (x, y) =>
+    val means = rows.map(row => row._2).reduce{ case (x, y) =>
       x.zip(y).map { case (x, y) => x + y }
     }.map(featureSum => featureSum / count)
 
-    val stddevs = rows.map(row =>
+    val stddevs = rows.map(row => row._2).map(row =>
       row.zip(means).map{ case (feature, mean) => scala.math.pow(feature - mean, 2) }
     ).reduce{ case (x, y) =>
       x.zip(y).map { case (x, y) => x + y }
     }.map(dev => scala.math.sqrt(dev / count))
 
     val rowsNorm = rows.map(row =>
-      row.zip(means).map{ case(feature, mean) => feature - mean }.zip(stddevs).map{ case(stage, stddev) => stage / stddev }
+      (row._1, row._2.zip(means).map{ case(feature, mean) => feature - mean }.zip(stddevs).map{ case(stage, stddev) => stage / stddev })
     ).collect()
 
     sc.stop()
 
     val pw = new PrintWriter(new File("output/creditcard_norm.csv"))
 
-    rowsNorm.map(row => row.mkString(",")).foreach(row => pw.println(row))
+    rowsNorm.map(row => row._2.mkString(",") + "," + row._1).foreach(row => pw.println(row))
     pw.close()
   }
 }
