0fe61e69e846e2174ac683d2c63f02d955da41f1
Enrique Sampaio dos Santos <developer@enriquesampaio.com>
2018-02-22 13:02:12 -0300

    Setting spark master

--- src/main/scala/com/enriquesampaio/frauddetection/FraudDetection.scala
+++ src/main/scala/com/enriquesampaio/frauddetection/FraudDetection.scala
@@ -8,11 +8,11 @@ object FraudDetection {
   def main(args: Array[String]): Unit = {
     val usage =
       """
-        |Usage: spark-submit frad-detection.jar --master masterNode normalize|sample|knn [case sample -> (train percentage) double] [case knn -> (k) int]
+        |Usage: spark-submit frad-detection.jar normalize|sample|knn [case sample -> (train percentage) double] [case knn -> (k) int]
       """.stripMargin
-    if (args.length == 0) println(usage)
+    if (args.length < 1) println(usage)
 
-    val conf = new SparkConf().setAppName("Fraud Detection")
+    val conf = new SparkConf().setAppName("Fraud Detection").setMaster("spark://localhost:7077")
     val sc = new SparkContext(conf)
 
     sc.setLogLevel("ERROR")
@@ -22,13 +22,13 @@ object FraudDetection {
         new Normalization("resources/creditcard.csv", "output/creditcard_norm.csv").normalize(sc)
       }
       case "sample" => {
-        if (args(1) !== null) {
+        if (args.length == 2) {
           new StratifiedSampling("resources/creditcard.csv", "output/creditcard_train.csv", "output/creditcard_test.csv", args(1).toDouble).stratify(sc)
         }
         new StratifiedSampling("resources/creditcard.csv", "output/creditcard_train.csv", "output/creditcard_test.csv", 0.7).stratify(sc)
       }
       case "knn" => {
-        if (args(1) !== null) {
+        if (args.length == 2) {
           println(new KNN(args(1).toInt, "output/creditcard_train.csv", "output/creditcard_test.csv").train(sc))
         }
         println(new KNN(3, "output/creditcard_train.csv", "output/creditcard_test.csv").train(sc))
